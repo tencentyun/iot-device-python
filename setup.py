9dac312261620d021e4a365941ba6dcc3eff2dc9
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-03-18 20:11:07 +0800

    添加test pipy环境
    Change-Id: I8ff7f9adbf001007788175ad8eed8b1f67936af4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import urllib.request
 import setuptools
 
 name = "TIoTExploreSDK"
-resp = urllib.request.urlopen(f'https://pypi.org/pypi/{name}/json')
+resp = urllib.request.urlopen(f'https://test.pypi.org/pypi/{name}/json')
 data = json.loads(resp.read().decode("utf-8"))
 version = data['info']['version']
 last_num = re.findall(r'\d+', version)[-1]
@@ -15,6 +15,7 @@ with open("README.md", mode='r', encoding='UTF-8') as fh:
 
 setuptools.setup(
     name=("%s" % name),
+#    version="0.0.1a01",
     version=f"{version}",
     author="larrytin",
     author_email="dev_tester@163.com",
