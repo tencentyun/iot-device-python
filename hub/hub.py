7a4f52e8819d4c2daf248e4e162d9db179340e32
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-03-25 15:08:54 +0800

    添加hub目录结构
    Change-Id: I831949b5ea09443b18b59c4da52a13f82cfed92e

---
+++ hub/hub.py
@@ -0,0 +1,32 @@
+#
+# Tencent is pleased to support the open source community by making IoT Hub available.
+# Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
+
+# Licensed under the MIT License (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://opensource.org/licenses/MIT
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is
+# distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import paho.mqtt.client as mqtt
+from Crypto.Cipher import AES
+
+class QcloudHub(object):
+
+    def __init__(self, tls=True):
+        self.__PahoLog = logging.getLogger("Paho")
+        self.__PahoLog.setLevel(logging.DEBUG)
+        pass
+
+    @property
+    def user_on_connect(self):
+        return self.__user_on_connect
+
+    @user_on_connect.setter
+    def user_on_connect(self, value):
+        self.__user_on_connect = value
+        pass
