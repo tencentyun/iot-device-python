472023557d4d2132d2c68ef818a5007c97a9d724
donghu-jiang <hdjiang@tencent.com>
2021-06-07 17:50:45 +0800

    代码重构:分层设计 	1.添加disconnect/publish/subscribe/unsubscribe接口 	2.a.提供mqtt设置接口;b.支持用户注册回调 	3.utils.py改为codec.py:提供编解码能力 	4.新增providers.py:初始化并输出静态变量 	5.a.调整explorer层接口;b.hub层支持explorer层注册回调;c.utils层功能封装 	6.a.explorer/hub新增services目录存放平台功能代码;b.hub层网关接口封装
    Change-Id: Iff73179f64f4026272d2a225167d0b974adffda0

---
+++ hub/protocol/protocol.py
@@ -0,0 +1,258 @@
+#
+# Tencent is pleased to support the open source community by making IoT Hub available.
+# Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
+
+# Licensed under the MIT License (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://opensource.org/licenses/MIT
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is
+# distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import sys
+import socket
+import string
+import time
+import hmac
+import hashlib
+import base64
+import random
+import ssl
+import paho.mqtt.client as mqtt
+from hub.utils.codec import Codec
+
+class AsyncConnClient(object):
+
+    def __init__(self, host, product_id, device_name, device_secret, websocket=False, tls=True, logger=None):
+        self.__logger = logger
+        self.__tls = tls
+        self.__useWebsocket = websocket
+        self.__key_mode = True
+        # self.__iot_ca_crt = self.__IOT_CA_CRT
+        self.__mqtt_client = None
+        self.__product_id = product_id
+        self.__device_name = device_name
+        self.__device_secret = device_secret
+        self.__host = host
+        self.__psk = None
+        self.__certificate = self.Certificate()
+        self.__codec = Codec()
+        self.__set_mqtt_default_param()
+
+        self.__init(host, product_id, device_name, device_secret)
+
+    class Certificate:
+        def __init__(self):
+            self.ca_file = None
+            self.cert_file = None
+            self.key_file = None
+
+    def __set_mqtt_default_param(self):
+        self.__mqtt_tls_port = 8883
+        self.__mqtt_tcp_port = 1883
+        self.__mqtt_socket_tls_port = 443
+        self.__mqtt_socket_tcp_port = 80
+        self.__mqtt_protocol = "MQTTv31"
+        self.__mqtt_transport = "TCP"
+        self.__mqtt_secure = "TLS"
+        self.__mqtt_clean_session = True
+        self.__mqtt_keep_alive = 60
+
+        self.__mqtt_auto_reconnect_min_sec = 1
+        self.__mqtt_auto_reconnect_max_sec = 60
+        self.__mqtt_max_queued_message = 40
+        self.__mqtt_max_inflight_message = 20
+        self.__mqtt_auto_reconnect_sec = 0
+        self.__mqtt_request_timeout = 10
+
+        # default MQTT/CoAP timeout value when connect/pub/sub
+        self.__mqtt_command_timeout = 5
+        pass
+
+    def _generate_pwss(self, client_id, device_secret):
+        self.__psk = base64.b64decode(device_secret.encode("utf-8"))
+
+        timestamp = str(int(round(time.time() * 1000)))
+        conn_id = ''.join(random.sample(string.ascii_letters + string.digits, 5))
+        username = client_id + ";21010406;" + conn_id + ";" + timestamp
+        sign = hmac.new(self.__psk, username.encode("utf-8"), hashlib.sha1).hexdigest()
+        password = "%s;hmacsha1" % (sign)
+
+        return username, password
+
+    def _ssl_init(self, key_mode):
+        # 密钥认证
+        if key_mode is True:
+            # context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH, cadata=self.__iot_ca_crt)
+            context = self.__codec.Ssl().create_content()
+            self.__mqtt_client.tls_set_context(context)
+        else:
+            ca = self.__certificate.ca_file
+            cert = self.__certificate.cert_file
+            key = self.__certificate.key_file
+            self.__mqtt_client.tls_set(ca_certs=ca, certfile=cert, keyfile=key,
+                                        cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_SSLv23)
+        pass
+
+    def __init(self, host, product_id, device_name, device_secret):
+        client_id = product_id + device_name
+        username, password = self._generate_pwss(client_id, device_secret)
+
+        if self.__mqtt_protocol == "MQTTv311":
+            mqtt_protocol_version = mqtt.MQTTv311
+        elif self.__mqtt_protocol == "MQTTv31":
+            mqtt_protocol_version = mqtt.MQTTv31
+        if self.__useWebsocket:
+            self.__mqtt_client = mqtt.Client(client_id=client_id,
+                                             clean_session=self.__mqtt_clean_session,
+                                             protocol=mqtt_protocol_version,
+                                             transport="websockets")
+        else:
+            self.__mqtt_client = mqtt.Client(client_id=client_id,
+                                             clean_session=self.__mqtt_clean_session,
+                                             protocol=mqtt_protocol_version)
+
+        # self._configuration_init(username, password)
+        self.__mqtt_client.username_pw_set(username, password)
+
+    def set_connect_state(self, state):
+        self.__connect_state = state
+
+    def get_connect_state(self):
+        return self.__connect_state
+    
+    def enable_logger(self, pahoLog):
+        if self.__mqtt_client is not None:
+            self.__mqtt_client.enable_logger(pahoLog)
+
+    # 设置重连时间,config_connect之前调用
+    def set_reconnect_interval(self, max_sec, min_sec):
+        self.__mqtt_auto_reconnect_min_sec = min_sec
+        self.__mqtt_auto_reconnect_max_sec = max_sec
+
+    # 设置消息发送超时时间,connect之前调用
+    def set_message_timout(self, timeout):
+        self.__mqtt_command_timeout = timeout
+
+    # 设置保活时间,connect之前调用
+    def set_keepalive_interval(self, interval):
+        self.__mqtt_keep_alive = interval
+
+    def reset_reconnect_wait(self):
+        self.__mqtt_auto_reconnect_sec = 0
+
+    def reconnect_wait(self):
+        if self.__mqtt_auto_reconnect_sec == 0:
+            self.__mqtt_auto_reconnect_sec = self.__mqtt_auto_reconnect_min_sec
+        else:
+            self.__mqtt_auto_reconnect_sec = min(self.__mqtt_auto_reconnect_sec * 2, self.__mqtt_auto_reconnect_max_sec)
+            self.__mqtt_auto_reconnect_sec += random.randint(1, self.__mqtt_auto_reconnect_sec)
+        time.sleep(self.__mqtt_auto_reconnect_sec)
+        pass
+
+    def register_event_callbacks(self, on_connect, on_disconnect, on_message, on_publish, on_subscribe, on_unsubscribe):
+        self.__mqtt_client.on_connect = on_connect
+        self.__mqtt_client.on_disconnect = on_disconnect
+        self.__mqtt_client.on_message = on_message
+        self.__mqtt_client.on_publish = on_publish
+        self.__mqtt_client.on_subscribe = on_subscribe
+        self.__mqtt_client.on_unsubscribe = on_unsubscribe
+
+    # 配置mqtt,connect之前调用
+    def config_connect(self):
+        self.__mqtt_client.reconnect_delay_set(self.__mqtt_auto_reconnect_min_sec, self.__mqtt_auto_reconnect_max_sec)
+        self.__mqtt_client.max_queued_messages_set(self.__mqtt_max_queued_message)
+        self.__mqtt_client.max_inflight_messages_set(self.__mqtt_max_inflight_message)
+
+    # 设置证书路径,connect之前调用
+    def set_cert_file(self, ca, cert, key):
+        # 认证模式置为证书方式
+        self.__key_mode = False
+        self.__certificate.ca_file = ca
+        self.__certificate.cert_file = cert
+        self.__certificate.key_file = key
+
+    def loop(self):
+        rc = mqtt.MQTT_ERR_SUCCESS
+        while rc == mqtt.MQTT_ERR_SUCCESS and self.__mqtt_client is not None:
+            rc = self.__mqtt_client.loop(self.__mqtt_command_timeout, 1)
+
+    def reconnect(self):
+        if self.__mqtt_client is not None:
+            self.__mqtt_client.reconnect()
+
+    def connect(self):
+        mqtt_port = self.__mqtt_tls_port
+        if self.__tls:
+            try:
+                if self.__useWebsocket:
+                    mqtt_port = self.__mqtt_socket_tls_port
+                self._ssl_init(self.__key_mode)
+            except ssl.SSLError as e:
+                self.__logger.error("ssl init error:" + str(e))
+                # self.__connect_state = self.ConnectState.INITIALIZED
+                # connect again 待添加
+                return False
+        else:
+            mqtt_port = self.__mqtt_tcp_port
+            if self.__useWebsocket:
+                mqtt_port = self.__mqtt_socket_tcp_port
+            pass
+        try:
+            self.__logger.debug("connect_async...%s", mqtt_port)
+            self.__mqtt_client.connect_async(host=self.__host, port=mqtt_port, keepalive=self.__mqtt_keep_alive)
+        except Exception as e:
+            self.__logger.error("mqtt connect with async error:" + str(e))
+            # self.__connect_state = self.ConnectState.INITIALIZED
+            # connect again 待添加
+            return False
+        return True
+
+    def disconnect(self):
+        if self.__mqtt_client is not None:
+            self.__mqtt_client.disconnect()
+
+    def subscribe(self, topic, qos=-1):
+        rc, mid = -1, -1
+        if self.__mqtt_client is None:
+            return rc, mid
+
+        if qos == -1:
+            rc, mid = self.__mqtt_client.subscribe(topic)
+        else:
+            rc, mid = self.__mqtt_client.subscribe(topic, qos)
+        if rc == mqtt.MQTT_ERR_SUCCESS:
+            self.__logger.debug("subscribe success topic:%s" % topic)
+            return 0, mid
+        elif rc == mqtt.MQTT_ERR_NO_CONN:
+            return 2, mid
+        else:
+            self.__logger.debug("subscribe error topic:%s" % topic)
+            return -1, mid
+
+    def unsubscribe(self, topic):
+        rc, mid = -1, -1
+        if self.__mqtt_client is None:
+            return rc, mid
+
+        rc, mid = self.__mqtt_client.unsubscribe(topic)
+        if rc == mqtt.MQTT_ERR_SUCCESS:
+            return 0, mid
+        else:
+            return rc, mid
+
+    def publish(self, topic, payload, qos):
+        rc, mid = -1, -1
+        if self.__mqtt_client is None:
+            return rc, mid
+        
+        rc, mid = self.__mqtt_client.publish(topic, payload, qos)
+        if rc == mqtt.MQTT_ERR_SUCCESS:
+            self.__logger.debug("publish success")
+            return 0, mid
+        else:
+            self.__logger.debug("publish failed")
+            return rc, mid
---
+++ hub/utils/providers.py
@@ -0,0 +1,229 @@
+#
+# Tencent is pleased to support the open source community by making IoT Hub available.
+# Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
+
+# Licensed under the MIT License (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://opensource.org/licenses/MIT
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is
+# distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+import string
+import json
+
+class TopicProvider(object):
+    def __init__(self, product_id, device_name):
+        self.__clientToken = None
+
+        # log topic
+        self.__log_topic_pub = "$log/operation/%s/%s" % (product_id, device_name)
+        self.__log_topic_sub = "$log/operation/result/%s/%s" % (product_id, device_name)
+        self.__is_subscribed_log_topic = False
+
+        # system topic
+        self.__sys_topic_pub = "$sys/operation/%s/%s" % (product_id, device_name)
+        self.__sys_topic_sub = "$sys/operation/result/%s/%s" % (product_id, device_name)
+
+        # gateway topic
+        self.__gateway_topic_pub = "$gateway/operation/%s/%s" % (product_id, device_name)
+        self.__gateway_topic_sub = "$gateway/operation/result/%s/%s" % (product_id, device_name)
+
+        # data template topic
+        self.__template_topic_pub = "$template/operation/%s/%s" % (product_id, device_name)
+        self.__template_topic_sub = "$template/operation/result/%s/%s" % (product_id, device_name)
+
+        # thing topic
+        self.__thing_property_topic_pub = "$thing/up/property/%s/%s" % (product_id, device_name)
+        self.__thing_property_topic_sub = "$thing/down/property/%s/%s" % (product_id, device_name)
+
+        self.__thing_action_topic_pub = "$thing/up/action/%s/%s" % (product_id, device_name)
+        self.__thing_action_topic_sub = "$thing/down/action/%s/%s" % (product_id, device_name)
+        self.__thing_event_topic_pub = "$thing/up/event/%s/%s" % (product_id, device_name)
+        self.__thing_event_topic_sub = "$thing/down/event/%s/%s" % (product_id, device_name)
+        self.__thing_raw_topic_pub = "$thing/up/raw/%s/%s" % (product_id, device_name)
+        self.__thing_raw_topic_sub = "$thing/down/raw/%s/%s" % (product_id, device_name)
+        self.__thing_service_topic_pub = "$thing/up/service/%s/%s" % (product_id, device_name)
+        self.__thing_service_topic_sub = "$thing/down/service/%s/%s" % (product_id, device_name)
+
+        # ota topic
+        self.__ota_report_topic_pub = "$ota/report/%s/%s" % (product_id, device_name)
+        self.__ota_update_topic_sub = "$ota/update/%s/%s" % (product_id, device_name)
+
+        # rrpc topic
+        self.__rrpc_topic_pub_prefix = "$rrpc/txd/%s/%s/" % (product_id, device_name)
+        self.__rrpc_topic_sub_prefix = "$rrpc/rxd/%s/%s/" % (product_id, device_name)
+
+        # shadow
+        self.__shadow_topic_pub = "$shadow/operation/%s/%s" % (product_id, device_name)
+        self.__shadow_topic_sub = "$shadow/operation/result/%s/%s" % (product_id, device_name)
+
+        # broadcast
+        self.__broadcast_topic_sub = "$broadcast/rxd/%s/%s" % (product_id, device_name)
+        pass
+
+    @property
+    def sys_topic_sub(self):
+        return self.__sys_topic_sub
+
+    @property
+    def sys_topic_pub(self):
+        return self.__sys_topic_pub
+
+    @property
+    def gateway_topic_sub(self):
+        return self.__gateway_topic_sub
+
+    @property
+    def gateway_topic_pub(self):
+        return self.__gateway_topic_pub
+
+    @property
+    def template_topic_sub(self):
+        return self.__template_topic_sub
+
+    @property
+    def template_event_topic_sub(self):
+        return self.__thing_event_topic_sub
+
+    @property
+    def template_event_topic_pub(self):
+        return self.__thing_event_topic_pub
+
+    @property
+    def template_action_topic_sub(self):
+        return self.__thing_action_topic_sub
+
+    @property
+    def template_property_topic_sub(self):
+        return self.__thing_property_topic_sub
+
+    @property
+    def template_property_topic_pub(self):
+        return self.__thing_property_topic_pub
+
+    @property
+    def template_action_topic_pub(self):
+        return self.__thing_action_topic_pub
+
+    @property
+    def template_service_topic_sub(self):
+        return self.__thing_service_topic_sub
+
+    @property
+    def template_raw_topic_sub(self):
+        return self.__thing_raw_topic_sub
+
+    @property
+    def ota_update_topic_sub(self):
+        return self.__ota_update_topic_sub
+
+    @property
+    def ota_report_topic_pub(self):
+        return self.__ota_report_topic_pub
+
+    @property
+    def rrpc_topic_pub_prefix(self):
+        return self.__rrpc_topic_pub_prefix
+
+    @property
+    def rrpc_topic_sub_prefix(self):
+        return self.__rrpc_topic_sub_prefix
+
+    @property
+    def shadow_topic_pub(self):
+        return self.__shadow_topic_pub
+
+    @property
+    def shadow_topic_sub(self):
+        return self.__shadow_topic_sub
+
+    @property
+    def broadcast_topic_sub(self):
+        return self.__broadcast_topic_sub
+
+    @property
+    def control_clientToken(self):
+        return self.__clientToken
+
+    # _on_template_downstream_topic_handler()中收到云端消息后保存client-token
+    @control_clientToken.setter
+    def control_clientToken(self, token):
+        if token is None or len(token) == 0:
+            raise ValueError('Invalid info.')
+        self.__clientToken = token
+
+class DeviceInfoProvider(object):
+    def __init__(self, file_path, logger=None):
+        self.__logger = logger
+        self.__logger.info('device_info file {}'.format(file_path))
+
+        self.__auth_mode = None
+        self.__device_name = None
+        self.__product_id = None
+        self.__product_secret = None
+        self.__device_secret = None
+        self.__ca_file = None
+        self.__cert_file = None
+        self.__private_key_file = None
+        self.__region = None
+        if self.__logger is not None:
+            self.__logger.info('device_info file {}'.format(file_path))
+        with open(file_path, 'r', encoding='utf-8') as f:
+            self.__json_data = json.loads(f.read())
+            self.__auth_mode = self.__json_data['auth_mode']
+            self.__device_name = self.__json_data['deviceName']
+            self.__product_id = self.__json_data['productId']
+            self.__product_secret = self.__json_data['productSecret']
+            self.__device_secret = self.__json_data['key_deviceinfo']['deviceSecret']
+            self.__ca_file = self.__json_data['cert_deviceinfo']['devCaFile']
+            self.__cert_file = self.__json_data['cert_deviceinfo']['devCertFile']
+            self.__private_key_file = self.__json_data['cert_deviceinfo']['devPrivateKeyFile']
+            self.__region = self.__json_data["region"]
+        if self.__logger is not None:
+            self.__logger.info(
+                "device name: {}, product id: {}, product secret: {}, device secret: {}".
+                format(self.__device_name, self.__product_id,
+                        self.__product_secret, self.__device_secret))
+
+    @property
+    def auth_mode(self):
+        return self.__auth_mode
+
+    @property
+    def device_name(self):
+        return self.__device_name
+
+    @property
+    def product_id(self):
+        return self.__product_id
+
+    @property
+    def product_secret(self):
+        return self.__product_secret
+
+    @property
+    def device_secret(self):
+        return self.__device_secret
+
+    @property
+    def ca_file(self):
+        return self.__ca_file
+
+    @property
+    def cert_file(self):
+        return self.__cert_file
+
+    @property
+    def private_key_file(self):
+        return self.__private_key_file
+
+    @property
+    def region(self):
+        return self.__region
+
+    @property
+    def json_data(self):
+        return self.__json_data
