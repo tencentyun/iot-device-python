9363254812c57727ef307f64db9bd0e4faa78da9
donghu-jiang <hdjiang@tencent.com>
2021-07-07 17:09:57 +0800

    调整rrpc sample
    Change-Id: I75f85a91c2527ab8746c38cd90264f5a76f5f414

--- hub/sample/rrpc/example_rrpc.py
+++ hub/sample/rrpc/example_rrpc.py
@@ -1,51 +1,68 @@
 import sys
 import logging
 import time
+import json
 from hub.hub import QcloudHub
+
+prduct_id = None
+device_name = None
+rrpc_reply = False
+te = None
 
 def on_connect(flags, rc, userdata):
     print("%s:flags:%d,rc:%d,userdata:%s" % (sys._getframe().f_code.co_name, flags, rc, userdata))
     pass
 
-
 def on_disconnect(rc, userdata):
     print("%s:rc:%d,userdata:%s" % (sys._getframe().f_code.co_name, rc, userdata))
     pass
 
-
-def on_message(topic, payload, qos, userdata):
+def on_message(topic, qos, payload, userdata):
     print("%s:topic:%s,payload:%s,qos:%s,userdata:%s" % (sys._getframe().f_code.co_name, topic, payload, qos, userdata))
     pass
-
 
 def on_publish(mid, userdata):
     print("%s:mid:%d,userdata:%s" % (sys._getframe().f_code.co_name, mid, userdata))
     pass
 
-
 def on_subscribe(mid, granted_qos, userdata):
     print("%s:mid:%d,granted_qos:%s,userdata:%s" % (sys._getframe().f_code.co_name, mid, granted_qos, userdata))
     pass
-
 
 def on_unsubscribe(mid, userdata):
     print("%s:mid:%d,userdata:%s" % (sys._getframe().f_code.co_name, mid, userdata))
     pass
 
+def on_rrpc_cb(topic, qos, payload, userdata):
+    print("%s:payload:%s,userdata:%s" % (sys._getframe().f_code.co_name, payload, userdata))
 
-def example_mqtt():
+    global prduct_id
+    global device_name
+    global te
+    te.rrpcReply(prduct_id, device_name, "ok", 2)
+    global rrpc_reply
+    rrpc_reply = True
+    pass
+
+def example_rrpc():
     __log_format = '%(asctime)s.%(msecs)03d [%(filename)s:%(lineno)d] - %(levelname)s - %(message)s'
     logging.basicConfig(format=__log_format)
 
-    te = QcloudHub(device_file="sample/device_info.json", userdata="",tls=True)
+    global prduct_id
+    global device_name
+    global te
 
-    print("\033[1;36m mqtt test start...\033[0m")
+    te = QcloudHub(device_file="sample/device_info.json", tls=True)
+    te.enableLogger(logging.DEBUG)
+
+    print("\033[1;36m shadow test start...\033[0m")
+
+    prduct_id = te.getProductID()
+    device_name = te.getDeviceName()
 
     te.registerMqttCallback(on_connect, on_disconnect,
                             on_message, on_publish,
                             on_subscribe, on_unsubscribe)
-
-    te.enableLogger(logging.DEBUG)
     te.connect()
 
     count = 0
@@ -61,7 +78,11 @@ def example_mqtt():
             time.sleep(1)
             count += 1
 
-    print("\033[1;36m mqtt test success...\033[0m")
+    te.rrpcInit(prduct_id, device_name, on_rrpc_cb)
+
+    # while rrpc_reply is False:
+    #     print("rrpc while...")
+    #     time.sleep(1)
+
+    print("\033[1;36m shadow test success...\033[0m")
     return True
-if __name__ == '__main__':
-	example_mqtt()
