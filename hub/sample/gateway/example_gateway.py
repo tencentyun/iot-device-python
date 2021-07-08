b2dbf343fe91de218822dbcf5f45b72d31d9400d
donghu-jiang <hdjiang@tencent.com>
2021-07-08 20:08:56 +0800

    调整hub网关sample
    Change-Id: I2ab7581a873a6de5b780d18881af5737b83c24ae

--- hub/sample/gateway/example_gateway.py
+++ hub/sample/gateway/example_gateway.py
@@ -2,28 +2,7 @@ import sys
 import time
 import logging
 from threading import Thread
-# explorer网关测试
-from explorer import explorer
-# hub网关测试
-# from hub.hub import QcloudHub
-
-from gateway import product_ZPHBLEB4J5 as product_ZPHBLEB4J5
-from gateway import product_Z53CXC198M as product_Z53CXC198M
-
-g_property_params = None
-g_control_msg_arrived = False
-
-g_task_1_runing = False
-g_task_2_runing = False
-g_task_1 = None
-g_task_2 = None
-
-# 网关下所有产品名称列表
-g_product_list = ["ZPHBLEB4J5", "Z53CXC198M"]
-
-# 产品下所有设备列表
-g_Z53CXC198M_subdev_list = []
-g_ZPHBLEB4J5_subdev_list = []
+from hub.hub import QcloudHub
 
 
 def on_connect(flags, rc, userdata):
@@ -55,38 +34,50 @@ def on_unsubscribe(mid, userdata):
     print("%s:mid:%d,userdata:%s" % (sys._getframe().f_code.co_name, mid, userdata))
     pass
 
+def on_subdev_cb(topic, qos, payload, userdata):
+    print("%s:topic:%s,payload:%s" % (sys._getframe().f_code.co_name, topic, payload))
+    pass
 
-# 调用数据模板入口函数
-def task_1(product_id, subdev_list=[]):
+def subscribe_subdev_topic(product_id, device_name, topic_suffix):
+    """
+    订阅网关子设备topic
+    eg:${productId}/${deviceName}/data
+    """
+    topic_list = []
+    topic_format = "%s/%s/%s"
+    topic_data = topic_format % (product_id, device_name, topic_suffix)
+    topic_list.append((topic_data, 0))
+    """ 注册topic对应回调 """
+    te.registerUserCallback(topic_data, on_subdev_cb)
 
-    global te
-    product_ZPHBLEB4J5.product_init(product_id, subdev_list, te)
+    """ 订阅子设备topic,在此必须传入元组列表[(topic1,qos2),(topic2,qos2)] """
+    rc, mid = te.gatewaySubdevSubscribe(topic_list)
+    if rc == 0:
+        print("gateway subdev subscribe success")
+    else:
+        print("gateway subdev subscribe fail")
+    pass
 
-    global g_task_1_runing
-    g_task_1_runing = True
-
-
-def task_2(product_id, subdev_list=[]):
-
-    global te
-    product_Z53CXC198M.product_init(product_id, subdev_list, te)
-
-    global g_task_2_runing
-    g_task_2_runing = True
-
+def publish_subdev_message(product_id, device_name, topic_suffix):
+    topic_format = "%s/%s/%s"
+    topic_data = topic_format % (product_id, device_name, topic_suffix)
+    message = {
+        "action":"gateway subdev publish"
+    }
+    te.publish(topic_data, message, 1)
 
 def example_gateway():
-    global g_task_1
-    global g_task_2
-
     __log_format = '%(asctime)s.%(msecs)03d [%(filename)s:%(lineno)d] - %(levelname)s - %(message)s'
     logging.basicConfig(format=__log_format)
 
     global te
-    te = explorer.QcloudExplorer(device_file="sample/device_info.json")
-    te.enableLogger(logging.WARNING)
+    te = QcloudHub(device_file="sample/device_info-wg.json")
+    te.enableLogger(logging.DEBUG)
 
     print("\033[1;36m gateway test start...\033[0m")
+
+    product_id = te.getProductID()
+    device_name = te.getDeviceName()
 
     te.registerMqttCallback(on_connect, on_disconnect,
                             on_message, on_publish,
@@ -111,6 +102,7 @@ def example_gateway():
 
     subdev_list = te.gatewaySubdevGetConfigList()
 
+    """
     while True:
         try:
             msg = input()
@@ -119,26 +111,31 @@ def example_gateway():
         else:
             if msg == "1":
                 for subdev in subdev_list:
-                    if te.isSubdevStatusOnline(subdev.sub_productId, subdev.sub_devName) is not True:
-                        rc, mid = te.gatewaySubdevOnline(subdev.sub_productId, subdev.sub_devName)
+                    print("name:%s" % subdev.device_name)
+                    if te.isSubdevStatusOnline(subdev.product_id, subdev.device_name) is not True:
+                        rc, mid = te.gatewaySubdevOnline(subdev.product_id, subdev.device_name)
                         if rc == 0:
-                            te.updateSubdevStatus(subdev.sub_productId, subdev.sub_devName, "online")
+                            te.updateSubdevStatus(subdev.product_id, subdev.device_name, "online")
+                            subscribe_subdev_topic(subdev.product_id, subdev.device_name, "data")
+                            publish_subdev_message(subdev.product_id, subdev.device_name, "data")
                             print("online success")
                         else:
                             print("online fail")
 
             elif msg == "2":
                 for subdev in subdev_list:
-                    if te.isSubdevStatusOnline(subdev.sub_productId, subdev.sub_devName) is True:
-                        rc, mid = te.gatewaySubdevOffline(subdev.sub_productId, subdev.sub_devName)
+                    if te.isSubdevStatusOnline(subdev.product_id, subdev.device_name) is True:
+                        rc, mid = te.gatewaySubdevOffline(subdev.product_id, subdev.device_name)
                         if rc == 0:
-                            te.updateSubdevStatus(subdev.sub_productId, subdev.sub_devName, "offline")
+                            te.updateSubdevStatus(subdev.product_id, subdev.device_name, "offline")
                             print("offline success")
                         else:
                             print("offline fail")
 
             elif msg == "3":
-                rc, mid = te.gatewaySubdevBind("SUBDEV_PRODUCT_ID", "SUBDEV_DEVICE_NAME", "SUBDEV_DEVICE_SECRET")
+                rc, mid = te.gatewaySubdevBind("YOUR_SUBDEV_PRODUCT_ID",
+                                                "YOUR_SUBDEV_DEVICE_NAME",
+                                                "YOUR_SUBDEV_SECRET")
                 if rc == 0:
                     print("bind success")
                 else:
@@ -152,45 +149,18 @@ def example_gateway():
                     print("unbind fail")
 
             elif msg == "5":
-                if not g_task_1_runing:
-                    product_id = g_product_list[0]
-                    for subdev in subdev_list:
-                        if subdev.sub_productId == product_id:
-                            g_ZPHBLEB4J5_subdev_list.append(subdev.sub_devName)
-                        else:
-                            continue
-
-                    # global g_task_1
-                    g_task_1 = Thread(target=task_1, args=(product_id, g_ZPHBLEB4J5_subdev_list,))
-                    g_task_1.start()
-
-                if not g_task_2_runing:
-                    product_id = g_product_list[1]
-                    for subdev in subdev_list:
-                        if subdev.sub_productId == product_id:
-                            g_Z53CXC198M_subdev_list.append(subdev.sub_devName)
-                        else:
-                            continue
-
-                    # global g_task_2
-                    g_task_2 = Thread(target=task_2, args=(product_id, g_Z53CXC198M_subdev_list,))
-                    g_task_2.start()
+                bind_list = []
+                bind_list = te.gatewaySubdevGetBindList(product_id, device_name)
+                for subdev in bind_list:
+                    print("subdev id:%s, name:%s" % (subdev.product_id, subdev.device_name))
 
             elif msg == "6":
                 te.disconnect()
-                # global g_task_1
-                if g_task_1.is_alive():
-                    g_task_1.stop()
-                    g_task_1.join()
-
-                # global g_task_2
-                if g_task_2.is_alive():
-                    g_task_2.stop()
-                    g_task_2.join()
 
             else:
                 sys.exit()
+    """
     print("\033[1;36m gateway test success...\033[0m")
     return True
-if __name__ == '__main__':
-	example_gateway()
+# if __name__ == '__main__':
+# 	example_gateway()
