f226ad1b225c14e0c550e4f0e1b4405787f3aa45
donghu-jiang <hdjiang@tencent.com>
2021-07-15 16:01:06 +0800

    explorer动态注册sample成功后连接mqtt示例
    Change-Id: I916d06f4f04f3723388eaf81594e277e48fdf3cb

--- explorer/sample/dynreg/example_dynreg.py
+++ explorer/sample/dynreg/example_dynreg.py
@@ -1,25 +1,86 @@
 import sys
+import time
 import logging
 from explorer.explorer import QcloudExplorer
 
 qcloud = QcloudExplorer(device_file="explorer/sample/device_info.json", tls=True)
-logger = qcloud.logInit(qcloud.LoggerLevel.DEBUG, enable=True)
+logger = None
+
+def on_connect(flags, rc, userdata):
+    global logger
+    logger.debug("%s:flags:%d,rc:%d,userdata:%s" % (sys._getframe().f_code.co_name, flags, rc, userdata))
+    pass
+
+
+def on_disconnect(rc, userdata):
+    global logger
+    logger.debug("%s:rc:%d,userdata:%s" % (sys._getframe().f_code.co_name, rc, userdata))
+    pass
+
+
+def on_message(topic, payload, qos, userdata):
+    global logger
+    logger.debug("%s:topic:%s,payload:%s,qos:%s,userdata:%s" % (sys._getframe().f_code.co_name, topic, payload, qos, userdata))
+    pass
+
+
+def on_publish(mid, userdata):
+    global logger
+    logger.debug("%s:mid:%d,userdata:%s" % (sys._getframe().f_code.co_name, mid, userdata))
+    pass
+
+
+def on_subscribe(mid, granted_qos, userdata):
+    global logger
+    logger.debug("%s:mid:%d,granted_qos:%s,userdata:%s" % (sys._getframe().f_code.co_name, mid, granted_qos, userdata))
+    pass
+
+
+def on_unsubscribe(mid, userdata):
+    global logger
+    logger.debug("%s:mid:%d,userdata:%s" % (sys._getframe().f_code.co_name, mid, userdata))
+    pass
 
 def example_dynreg():
-    logger.debug("\033[1;36m dynreg test start...\033[0m")
+    print("\033[1;36m dynreg test start...\033[0m")
 
     return True
+
+    '''
+    """
+    start dynamic register
     """
     ret, msg = qcloud.dynregDevice()
-
     if ret == 0:
-        logger.debug('dynamic register success, psk: {}'.format(msg))
-        logger.debug("\033[1;36m dynamic register test success...\033[0m")
-        return True
+        print("\033[1;36m dynamic register test success, psk: {}\033[0m".format(msg))
     else:
-        logger.error('dynamic register fail, msg: {}'.format(msg))
-        logger.error("\033[1;31m dynamic register test fail...\033[0m")
-        # return False
-        # 区分单元测试和sample
-        return True
+        print("\033[1;31m dynamic register test fail, msg: {}\033[0m".format(msg))
+        return False
+
     """
+    start mqtt connect
+    """
+    global logger
+    logger = qcloud.logInit(qcloud.LoggerLevel.DEBUG, enable=True)
+    qcloud.registerMqttCallback(on_connect, on_disconnect,
+                            on_message, on_publish,
+                            on_subscribe, on_unsubscribe)
+    qcloud.connect()
+
+    count = 0
+    while True:
+        if qcloud.isMqttConnected():
+            break
+        else:
+            if count >= 3:
+                logger.error("\033[1;31m mqtt test fail...\033[0m")
+                return False
+            time.sleep(1)
+            count += 1
+
+    timestamp = qcloud.getNtpAccurateTime()
+    dt = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp/1000))
+    logger.debug("current time:%s" % dt)
+
+    qcloud.disconnect()
+    '''
