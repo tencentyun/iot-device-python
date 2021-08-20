d1176e51d8767295290bea2f9bb151604a0b5226
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-08-20 16:26:53 +0800

    setup配置传递依赖，修改sdk名称
    Change-Id: I6cb4c1c84606b01dffbbbd0148798905994da8d3

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ import re
 import urllib.request
 import setuptools
 
-name = "TIoTExploreSDK"
-resp = urllib.request.urlopen(f'https://test.pypi.org/pypi/{name}/json')
-data = json.loads(resp.read().decode("utf-8"))
-version = data['info']['version']
-last_num = re.findall(r'\d+', version)[-1]
-version = version[:-len(last_num)] + str(int(last_num) + 1)
+name = "tencent-iot-device"
+# resp = urllib.request.urlopen(f'https://pypi.org/pypi/{name}/json')
+# resp = urllib.request.urlopen(f'https://test.pypi.org/pypi/{name}/json')
+# data = json.loads(resp.read().decode("utf-8"))
+# version = data['info']['version']
+# last_num = re.findall(r'\d+', version)[-1]
+# version = version[:-len(last_num)] + str(int(last_num) + 1)
 
 with open("README.md", mode='r', encoding='UTF-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name=("%s" % name),
-#    version="0.0.1a01",
-    version=f"{version}",
+    version="1.0.0",
+    # version=f"{version}",
     author="larrytin",
     author_email="dev_tester@163.com",
     description="Tencent IoT Explorer SDK for Python",
@@ -24,6 +25,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/tencentyun/iot-device-python",
     packages=setuptools.find_packages(),
+    install_requires = ["paho-mqtt==1.5.1", "pycrypto==2.6.1"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
